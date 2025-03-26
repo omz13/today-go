7ad9122c980ffd6133a7a0bd43865ed8f3df3835
David Somers <dsomers@omz13.com>
2025-03-26 14:32:46 +0100

    build: add license

--- package.go
+++ package.go
@@ -1,3 +1,3 @@
-// package today is used to answer the eternal question: is today the day (for this anniversary event)
-
+// Package today is used to answer the eternal question: is today the day (for an
+// event that happens on a certain date each year).
 package today
