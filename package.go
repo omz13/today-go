ba91be62c3cf6a75b5c16dfbb979af8a9993da1a
David Somers <dsomers@omz13.com>
2025-03-31 12:08:26 +0200

    docs: add exposition

--- package.go
+++ package.go
@@ -1,3 +1,12 @@
 // Package today is used to answer the eternal question: is today the day (for an
 // event that happens on a certain date each year).
+//
+// For a limited set of named events, helpers are available, i.a. BeCssNakedDay
+// per [https://css-naked-day.github.io/]; BeJsNakedDay per
+// [https://js-naked-day.org/]; BeTowelDay per
+// [https://en.wikipedia.org/wiki/Towel_Day].
+//
+// Otherwise, IsInternationalDay indicates if a requested month and day is
+// occurring now anywhere on Earth, or its sibling IsDayMonth for occurrence in
+// the current timezone.
 package today
